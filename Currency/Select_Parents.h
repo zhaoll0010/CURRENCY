e597095f9a11987d2f1c77efb3ce13db6630de91
alexhiro <renjiec@utas.edu.au>
2015-12-01 01:41:23 +1100

    Create Select_Parents.h

---
+++ Currency/Select_Parents.h
@@ -0,0 +1,9 @@
+#ifndef _INCL_SELECT_PARENTS
+#define _INCL_SELECT_PARENTS
+#include "Chromosome.h"
+class Select_Parents
+{
+public:
+	static void selectParents(Chromosome **cp, int *i, int *j);
+};
+#endif
