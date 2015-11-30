9b8c00d433b06c031d35852b801a301bd3f053f1
alexhiro <renjiec@utas.edu.au>
2015-11-30 14:54:25 +1100

    Update Buy_Sell_Rule.cpp

--- Currency/Buy_Sell_Rule.cpp
+++ Currency/Buy_Sell_Rule.cpp
@@ -9,7 +9,16 @@ void Buy_Sell_Rule::operator=(Buy_Sell_Rule &r)
 	}
 }
 
-int Buy_Sell_Rule::Random_Int(int max)
+int Buy_Sell_Rule::Random_Max_Bit(int bit)
 {
+	int max = 1 << bit;
 	return (rand() * max / RAND_MAX);
+}
+
+void Buy_Sell_Rule::Generate_Random()
+{
+	for (int i = 0; i < NUMBER1; i++)
+	{
+		gen1[i] = Random_Max_Bit(GEN_LEN1);
+	}
 }
