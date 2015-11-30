30c2720b5eee2738af80261ed4587cb2e36d7655
alexhiro <renjiec@utas.edu.au>
2015-11-30 15:24:37 +1100

    Update Main.cpp

--- Currency/Main.cpp
+++ Currency/Main.cpp
@@ -1,6 +1,36 @@
 #include "Chromosome.h"
+#define INITIAL_NUMBER 100
+
+int fitness(Chromosome &c)
+{
+	return 0;
+}
 
 void main()
 {
-	Chromosome c;
+	Chromosome** cp;
+	Chromosome** c2;
+	int fit[INITIAL_NUMBER];
+	
+	cp = new Chromosome*[100];
+	c2 = new Chromosome*[100];
+	
+	for(int i = 0; i < INITIAL_NUMBER; i++)
+	{
+		cp[i] = new Chromosome();
+		fit[i] = fitness(*cp[i]);
+	}
+	
+	//sort(fit[i], cp[i])
+	
+	{
+		//c2[i] = new Chromosome(*cp[a]);
+		//c2[j] = new Chromosome(*cp[b]);
+		//c2[i]->Crossover(*c2[j]);
+		//c2[i]->Mutation();
+		//c2[j]->Mutation();
+		//fit[i] = fitness(*c2[i]);
+		//fit[j] = fitness(*c2[j]);
+	}
+	
 }
