2cbb7e11a55393e93f9ffef7a0b1d56526e5c62d
alexhiro <renjiec@utas.edu.au>
2015-11-30 14:56:53 +1100

    Update Common_Rule.cpp

--- Currency/Common_Rule.cpp
+++ Currency/Common_Rule.cpp
@@ -16,3 +16,25 @@ void Common_Rule::operator=(Common_Rule &r)
 		gen3[i] = r.gen3[i];
 	}
 }
+
+int Common_Rule::Random_Max_Bit(int bit)
+{
+	int max = 1 << bit;
+	return (rand() * max / RAND_MAX);
+}
+
+void Common_Rule::Generate_Random()
+{
+	for (int i = 0; i < NUMBER1; i++)
+	{
+		gen1[i] = Random_Max_Bit(GEN_LEN1);
+	}
+	for (int i = 0; i < NUMBER2; i++)
+	{
+		gen2[i] = Random_Max_Bit(GEN_LEN2);
+	}
+	for (int i = 0; i < NUMBER3; i++)
+	{
+		gen3[i] = Random_Max_Bit(GEN_LEN3);
+	}
+}
