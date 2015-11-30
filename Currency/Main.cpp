1477e2be5ff2a23df6c0846174c7c1b513b63b5f
alexhiro <renjiec@utas.edu.au>
2015-11-30 15:35:35 +1100

    Update Main.cpp

--- Currency/Main.cpp
+++ Currency/Main.cpp
@@ -21,16 +21,30 @@ void main()
 		fit[i] = fitness(*cp[i]);
 	}
 	
-	//sort(fit[i], cp[i])
 	
+	//while(1)
 	{
-		//c2[i] = new Chromosome(*cp[a]);
-		//c2[j] = new Chromosome(*cp[b]);
-		//c2[i]->Crossover(*c2[j]);
-		//c2[i]->Mutation();
-		//c2[j]->Mutation();
-		//fit[i] = fitness(*c2[i]);
-		//fit[j] = fitness(*c2[j]);
+		//sort(fit[i], cp[i])
+	
+		for(int n = 0; n < INITIAL_NUMBER; n += 2)
+		{
+			//int i, j;
+			//getParents(cp, &i, &j);
+			//c2[i] = new Chromosome(*cp[a]);
+			//c2[j] = new Chromosome(*cp[b]);
+			//c2[i]->Crossover(*c2[j]);
+			//c2[i]->Mutation();
+			//c2[j]->Mutation();
+			//fit[i] = fitness(*c2[i]);
+			//fit[j] = fitness(*c2[j]);
+		}
+		
+		for(int i = 0; i < INITIAL_NUMBER; i++)
+		{
+			delete cp[i];
+			cp[i] = new Chromosome(*c2[i]);
+		}
 	}
+	
 	
 }
